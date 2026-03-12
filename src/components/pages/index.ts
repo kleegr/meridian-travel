fca67926787755b8b30f0a2a6b77365afde186af
kleegr <Info@kleegr.com>
2026-03-13 01:09:49 +0500

    chore: Add PrintView to page exports

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,6 +2,7 @@ export { default as Dashboard } from './Dashboard';
 export { default as BoardView } from './BoardView';
 export { default as ItineraryList } from './ItineraryList';
 export { default as ItineraryDetail } from './ItineraryDetail';
+export { default as PrintView } from './PrintView';
 export { default as Financials } from './Financials';
 export { default as Travelers } from './Travelers';
 export { default as Settings } from './Settings';
